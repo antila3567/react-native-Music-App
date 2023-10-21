3324a9543ad0c35a5bad7d5c3ad83ec8187c94fb
Иван Легенький <antila3567@gmail.com>
2023-10-21 11:13:45 +0300

    try another case with hook

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -7,5 +7,5 @@ export * from '@react-native-async-storage/async-storage/jest/async-storage-mock
 // Note: test renderer must be required after react-native.
 
 it('renders correctly', () => {
-  return 'okay';
+  return undefined;
 });
